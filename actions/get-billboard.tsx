1fe0caec73631a729b772e1eeab0ff1168222174
Onat <53895969+onattech@users.noreply.github.com>
2023-07-26 23:46:53 +0300

    Update get-billboard.tsx

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -1,11 +1,12 @@
 import { Billboard } from "@/types"
+import axios from "axios"
 
 const URL = `https://store-admin.onatim.com/api/05545a6b-0880-48b1-9ad9-797a5054bc10/billboards`
 
 const getBillboard = async (id: string): Promise<Billboard | undefined> => {
     try {
-        const res = await fetch(`${URL}/${id}`)
-        return res.json()
+        const res = await axios.get(`${URL}/${id}`)
+        return res.data
     } catch (error) {
         console.log("URL:", URL)
         console.log("ID:", id)
