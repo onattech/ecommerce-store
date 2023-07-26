5898c618c7064556a51e58816ee76427cede01fc
Onat <53895969+onattech@users.noreply.github.com>
2023-07-26 22:16:03 +0300

    Update page.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,19 +1,19 @@
-// import getBillboard from "@/actions/get-billboard"
+import getBillboard from "@/actions/get-billboard"
 // import getProducts from "@/actions/get-products"
 // import ProductList from "@/components/product-list"
-// import Billboard from "@/components/ui/billboard"
+import Billboard from "@/components/ui/billboard"
 import Container from "@/components/ui/container"
 
 export const revalidate = 0
 
 const HomePage = async () => {
     // const products = await getProducts({ isFeatured: true })
-    // const billboard = await getBillboard("b7279df3-3488-412b-8050-cf305cbf3a9d")
+    const billboard = await getBillboard("b7279df3-3488-412b-8050-cf305cbf3a9d")
 
     return (
         <Container>
             <div className="space-y-10 pb-10">
-                {/* <Billboard data={billboard} /> */}
+                <Billboard data={billboard} />
                 <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
                     hi
                     {/* <ProductList title="Featured Products" items={products} /> */}
