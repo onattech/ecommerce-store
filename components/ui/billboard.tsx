cb63b9a6a3b469523b45c21e6d8e801881b5c202
Onat <53895969+onattech@users.noreply.github.com>
2023-07-26 22:26:08 +0300

    1

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -8,6 +8,7 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
     if (!data) {
         return null
     }
+
     return (
         <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
             <div
